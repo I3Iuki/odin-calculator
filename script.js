38ac3e14f5a5a4c829a0d698ccae19308cfbfb6d
I3Iuki <nguyenkien20101011@gmail.com>
2026-01-03 19:50:26 -0500

    Coded most of the functions. Still need to add some event listeners, then make everything appear on the DOM. Will finish another time.

--- script.js
+++ script.js
@@ -1,58 +1,70 @@
-/* 
-functions: 
-    - numbers
-    - delete
-    - all clear
-    - addition
-    - subtraction
-    - multiplication
-    - division
-    - decimals
-*/
-
 const display = document.getElementById("display");
 const numerals = document.querySelectorAll(".numerals");
-const operations = document.getElementById("operations");
+const operations = document.querySelectorAll("operations");
 const prevAns = document.getElementById("previous-answer");
 const clearBtn = document.getElementById("clear");
 const clearAllBtn = document.getElementById("clear-all");
 const equal = document.getElementById("equal");
+const preview = document.getElementById("preview");
+const currentInput = document.getElementById("current-input")
 
-let firstNumber = "";
-let secondNumber = "";
+let firstNumber;
+let secondNumber;
+let operationSymbol;
 let currentOperation;
 
-function add(a, b) {
+let previewEquation = `${firstNumber} + ${operationSymbol} + ${secondNumber}`
 
+console.log(previewEquation)
+
+function add(a, b) {
+    return a + b;
 }
 
 function subtract(a, b) {
-    
+    return a - b
 }
 
 function multiply(a, b) {
-    
+    return a * b
 }
 
 function divide(a, b) {
-    
+    return a / b
 }
 
 function clear() {
-
+    if (secondNumber) {
+        secondNumber.split("").pop().join("");
+    } else if (operator) {
+        operator = "";
+    } else if (firstNumber) {
+        firstNumber.split("").pop().join("");
+    }
 }
 
 function clearAll() {
+    firstNumber = "";
+    secondNumber = "";
+    operationSymbol = "";
+    currentOperation = null;
 
 }
 
 function inputNumber(button) {
-
+    if (!currentOperation) {
+        firstNumber += button.innerHTML;
+    } else if (currentOperation) {
+        secondNumber += button.innerHTML;
+    }
 }
 
 function changeOperation() {
-
+    if (!currentOperation || currentOperation && !secondNumber) {
+        console.log("changed operation" + currentOperation + secondNumber);
+    }
 }
+
 
 function updateDisplay() {
 
@@ -73,5 +85,3 @@ for (button of operations) {
         changeOperation(e);
     });
 }
-
-prevAns
